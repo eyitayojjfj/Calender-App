3a392401594c9bcffe4179becc9ac09e711b7c32
eyitayojjfj <sulaimoneyitayo@gmail.com>
2024-10-15 20:28:34 +0100

    third-commit

--- src/components/CalenderApp.jsx
+++ src/components/CalenderApp.jsx
@@ -145,7 +145,7 @@ const CalenderApp = () => {
                             <input type="number" name='hours' min={0} max={24} className='hours' value={eventTime.hours} onChange={handleTimeChange} />
                             <input type="number" name='minutes' min={0} max={60} className='minutes' value={eventTime.minutes} onChange={handleTimeChange} />
                         </div>
-                        <textarea placeholder='Enter Event Text (Maximum 80 Characters)' value={eventText} onChange={(e) => { if (e.target.value.length <= 80) { setEventText(e.target.value) } }} />
+                        <textarea placeholder='Enter Event Text (Maximum 60 Characters)' value={eventText} onChange={(e) => { if (e.target.value.length <= 60) { setEventText(e.target.value) } }} />
                         <button className="event-popup-btn" onClick={handleEventSubmit}>{editingEvent ? 'Update Event' : 'Add Event'}</button>
                         <button className="close-event-popup" onClick={() => setShowEventPopup(false)}>
                             <i className="bx bx-x"></i>
